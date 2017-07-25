39d6079e255c9ae33ef639c6085baa110ce15fb9
tiago <si.tiagoferreira@gmail.com>
2017-07-24 22:18:25 -0300

    refactoring lists

--- basicDataTypes/lists.py
+++ basicDataTypes/lists.py
@@ -1,11 +1,12 @@
-if __name__ == '__main__':
-    N = int(raw_input())
-    values = list()
+def read_commands(N):
     lines = list()
-    commands = list()
     for i in range(0, N):
         lines.append(raw_input())
+    return lines
 
+def execute_commands(lines):
+    values = list()
+    commands = list()
     for i in range(0, len(lines)):
         commands = lines[i].split( )
         command = commands[0]
@@ -27,3 +28,8 @@ if __name__ == '__main__':
             values.pop()
         if(command == 'reverse'):
             values.reverse()
+
+if __name__ == '__main__':
+    N = int(raw_input())
+    lines = read_commands(N)
+    execute_commands(lines)
